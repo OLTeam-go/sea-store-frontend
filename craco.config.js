5b2e86bc746cf3fa62c4fb2d18af73abdead165a
komangyogananda <yoganandamahaputra@gmail.com>
2020-08-25 07:11:47 +0700

    init setup

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { "@primary-color": "#225560" },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
